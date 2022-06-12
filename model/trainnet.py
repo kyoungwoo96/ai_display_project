f8195858a032c025f31fac7dce617355fbc62926
lkw15 <tnt7986@gmail.com>
2022-06-13 03:16:28 +0900

    hello

--- model/trainnet.py
+++ model/trainnet.py
@@ -63,8 +63,8 @@ class MultiHeadAttention(nn.Module):
 
         output = output.view(n_head, sz_b, len_q, d_v)
         output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1)  # b x lq x (n*dv)
-
-        output = self.dropout(self.fc(output))
+        output = self.fc(output)
+        output = self.dropout(output)
         output = self.layer_norm(output + residual)
 
         return output
